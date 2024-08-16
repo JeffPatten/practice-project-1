b3c4b813e26821a7902eb827ee4b15c37d67ac72
Jeff Patten <jeffspatten@gmail.com>
2024-08-16 12:47:02 -0700

    add TabButton component

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TabButton = ({ children }) => {
+    const clickHandler = () => {
+
+    }
+
+  return (
+    <li>
+      <button onClick={clickHandler}>{children}</button>
+    </li>
+  );
+};
+
+export default TabButton;
